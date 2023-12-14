31e60ef8e25ae959df3ce9050b5ce6c9cb304cf9
YujiAsofu <48573877+YujiAsofu@users.noreply.github.com>
2023-12-14 16:29:17 -0300

    Add files via upload

---
+++ game/Game.java
@@ -0,0 +1,16 @@
+package game;
+
+public class Game {
+    public void startGame() {
+    }
+    public int checkWinner() {
+    }
+    public void newRound() {
+
+    }
+
+    public void endGame() {
+
+    }
+
+}
