a1b67d6871333d78dc1abfa7cd1ffb77f657e2b5
YujiAsofu <48573877+YujiAsofu@users.noreply.github.com>
2024-01-03 20:31:15 -0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import titan.Wheel;
+
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) {
+        titan.Wheel.defineResults();
+    }
+}
---
+++ src/game/Game.java
@@ -0,0 +1,13 @@
+package game;
+
+public class Game {
+    /* public void startGame() {
+    }
+    public int checkWinner();
+    public void newRound();
+
+    public void endGame() {
+
+    } */
+
+}
